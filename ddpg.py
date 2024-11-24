083c605fe1f256bf329400781170aad81d6fe556
catamay <akcopinga@gmail.com>
2024-11-24 12:37:29 -0500

    separated ddpg from the training loop

--- ddpg.py
+++ ddpg.py
@@ -2,9 +2,9 @@ import gymnasium as gym
 from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
 
 import math
-from time import localtime, strftime
 import os
 import random
+from tqdm import tqdm
 
 import torch
 import torch.nn as nn
@@ -12,20 +12,6 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 
 from collections import namedtuple, deque
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Hyperparameters
-N_EPISODES = 2500
-BATCH_SIZE = 128
-MEMORY_SIZE = 1000000
-GAMMA = 0.99
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 100
-WEIGHT_DECAY = 0.005
-TAU = 1e-3
-LR = 1e-4
 
 # Memory class
 # Used to create batches on which to train the agent
@@ -66,11 +52,13 @@ class Critic(nn.Module):
 
 # Deep Deterministic Policy Gradient
 class DDPG:
-    def __init__(self, n_obs, n_actions, n_hidden=300):
+    def __init__(self, n_obs, n_actions, batch_size, gamma, tau, lr, weight_decay, device=torch.device("cpu"), n_hidden=300):
         self.n_obs = n_obs
         self.n_actions = n_actions
         self.n_hidden = n_hidden
-        self.action_space = gym.make("HalfCheetah-v5").action_space
+        self.batch_size = batch_size
+        self.tau = tau
+        self.gamma = gamma
 
         self.actor_local = nn.Sequential(
                 nn.Linear(n_obs, n_hidden),
@@ -92,27 +80,25 @@ class DDPG:
                 nn.Linear(n_hidden, n_actions),
                 nn.Tanh()
             ).to(device)
-        self.actor_optim = optim.AdamW(self.actor_local.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+        self.actor_optim = optim.AdamW(self.actor_local.parameters(), lr=lr, weight_decay=weight_decay)
         
         self.critic_local = Critic(n_obs, n_hidden, n_actions).to(device)
         self.critic_target = Critic(n_obs, n_hidden, n_actions).to(device)
-        self.critic_optim = optim.AdamW(self.critic_local.parameters(), lr=LR, weight_decay=WEIGHT_DECAY)
+        self.critic_optim = optim.AdamW(self.critic_local.parameters(), lr=lr, weight_decay=weight_decay)
 
     def select_action(self, obs, eps=0):
-        if random.random() < eps:
-            return torch.tensor(self.action_space.sample(), device=device, dtype=torch.float32)
-        else:
-            self.actor_local.eval()
-            with torch.no_grad():
-                action = self.actor_local(obs).squeeze(0)
-            self.actor_local.train()
-            return action
+        self.actor_local.eval()
+        with torch.no_grad():
+            action = self.actor_local(obs).squeeze(0)
+        self.actor_local.train()
+        return action
+            
 
     def update(self, memory, criterion):
-        if len(memory) < BATCH_SIZE:
+        if len(memory) < self.batch_size:
             return 0 # no loss until we can make a batch
 
-        transitions = memory.sample(BATCH_SIZE)
+        transitions = memory.sample(self.batch_size)
         batch = Transition(*zip(*transitions))
 
         # Batch a sample of the memory
@@ -123,7 +109,7 @@ class DDPG:
 
         # Use critic to compute q value after actor action
         next_actions_batch = self.actor_target(next_state_batch)
-        target_q_values = reward_batch.unsqueeze(1) + GAMMA * self.critic_target(next_state_batch, next_actions_batch)
+        target_q_values = reward_batch.unsqueeze(1) + self.gamma * self.critic_target(next_state_batch, next_actions_batch)
 
         # Critic update
         q_values = self.critic_local(state_batch, action_batch)
@@ -147,14 +133,14 @@ class DDPG:
 
     def soft_update(self, local_model, target_model):
         for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-            target_param.data.copy_(TAU*local_param.data + (1.0-TAU)*target_param.data)
+            target_param.data.copy_(self.tau*local_param.data + (1.0-self.tau)*target_param.data)
 
-    def save(self, path='models'):
+    def save(self, name, path='models'):
         if 'models' in path and os.path.isdir('models') is False:
             os.mkdir('models')
         torch.save({'actor_weights': self.actor_local.state_dict(),
                     'critic_weights': self.critic_local.state_dict()
-                    }, f"{path}/{strftime('%Y-%m-%d %H:%M:%S', localtime())}")
+                    }, f"{path}/name.pt")
 
     def load(self, path):
         model_dict = torch.load(path)
@@ -162,35 +148,23 @@ class DDPG:
         self.actor_target.load_state_dict(model_dict['actor_weights'])
         self.critic_local.load_state_dict(model_dict['critic_weights'])
         self.critic_target.load_state_dict(model_dict['critic_weights'])
-                
 
-# Algorithm
-env = gym.make("HalfCheetah-v5", render_mode="rgb_array", max_episode_steps=200)
-
-n_actions = env.action_space.shape[0] 
-state, _ = env.reset()
-n_obs = len(state)
-
-# Training loop
-agent = DDPG(n_obs, n_actions)
-agent.load('models/2024-11-21 19:55:28')
-criterion = nn.MSELoss()
-memory = ReplayMemory(MEMORY_SIZE)
-losses = []
-
-for i_episode in range(N_EPISODES):
+def train(episode, eps_start, eps_end, eps_decay, criterion, agent, memory, env, device=torch.device("cpu")):
     state, info = env.reset()
     state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-    print(f"The current episode is: {i_episode}")
     episode_loss = 0
     episode_reward = 0
     while True:
         # Select an action in the current state and
         # add the resulting observations to the
         # memory buffer
-        eps = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * i_episode / EPS_DECAY)
-        action = agent.select_action(state, eps)
-        observation, reward, terminated, truncated, _ = env.step(action.cpu().numpy())
+        eps = eps_end + (eps_start - eps_end) * math.exp(-1. * episode / eps_decay)
+
+        if random.random()<eps:
+            action = torch.Tensor(env.action_space.sample())
+        else:
+            action = agent.select_action(state, eps)
+        observation, reward, terminated, truncated, _ = env.step(action.numpy(force=True))
         episode_reward += reward
         reward = torch.tensor([reward], device=device, dtype=torch.float32)
         done = terminated or truncated
@@ -202,33 +176,83 @@ for i_episode in range(N_EPISODES):
         agent.update(memory, criterion)
 
         if done:
-            print(f"Episode stats: reward = {episode_reward}")
-            losses.append(episode_reward)
-            break
+            return episode_reward            
 
-print("Training completed.")
-plt.plot(losses)
-plt.xlabel("Episodes")
-plt.ylabel("Training Reward")
-plt.show()
+# # Algorithm
+# env = gym.make("HumanoidStandup-v5", render_mode="rgb_array", max_episode_steps=200)
 
-print("Saving model...")
-agent.save()
-print("Saved Model Weights!")
+# n_actions = env.action_space.shape[0] 
+# state, _ = env.reset()
+# n_obs = len(state)
 
-num_eval_episodes=5
-env = RecordEpisodeStatistics(env, buffer_length=num_eval_episodes)
-env = RecordVideo(env, video_folder="video_renders", name_prefix="cheetah", episode_trigger=lambda _: True)
+# # Training loop
+# agent = DDPG(n_obs, n_actions)
+# criterion = nn.MSELoss()
+# memory = ReplayMemory(MEMORY_SIZE)
+# losses = []
 
-for episode_num in range(num_eval_episodes):
-    obs, _ = env.reset()
-    done = False
+# progress_bar = tqdm(total=N_EPISODES, position=0, leave=True)
+# for i_episode in range(N_EPISODES):
+#     progress_bar.update(1)
+#     state, info = env.reset()
+#     state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+#     episode_loss = 0
+#     episode_reward = 0
+#     while True:
+#         # Select an action in the current state and
+#         # add the resulting observations to the
+#         # memory buffer
+#         eps = EPS_END + (EPS_START - EPS_END) * math.exp(-1. * i_episode / EPS_DECAY)
 
-    while not done:
-        state = torch.tensor(obs, dtype=torch.float32, device=device).unsqueeze(0)
-        action = agent.select_action(state).cpu().numpy()
+#         if random.random()<eps:
+#             action = torch.Tensor(env.action_space.sample())
+#         else:
+#             action = agent.select_action(state, eps)
+#         observation, reward, terminated, truncated, _ = env.step(action.numpy(force=True))
+#         episode_reward += reward
+#         reward = torch.tensor([reward], device=device, dtype=torch.float32)
+#         done = terminated or truncated
 
-        obs, reward, terminated, truncated, info = env.step(action)
-        done = terminated or truncated
+#         next_state = None if terminated else torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+#         memory.push(state, action.unsqueeze(0), next_state, reward)
 
-env.close()
+#         state = next_state
+#         agent.update(memory, criterion)
+
+#         if done:
+#             losses.append(episode_reward)
+#             break
+#     if i_episode>0:
+#         progress_bar.set_postfix({
+#                 'last reward': round(episode_reward, 5),
+#                 'best reward': round(min(losses), 5),
+#                 })
+    
+# del progress_bar
+
+# print("Training completed.")
+# plt.plot(losses)
+# plt.xlabel("Episodes")
+# plt.ylabel("Training Reward")
+# plt.show()
+
+# print("Saving model...")
+# agent.save()
+# print("Saved Model Weights!")
+
+# num_eval_episodes=5
+# env = RecordEpisodeStatistics(env, buffer_length=num_eval_episodes)
+# env = RecordVideo(env, video_folder="video_renders", name_prefix="cheetah", episode_trigger=lambda _: True)
+
+# for episode_num in range(num_eval_episodes):
+#     obs, _ = env.reset()
+#     done = False
+
+#     while not done:
+#         state = torch.tensor(obs, dtype=torch.float32, device=device).unsqueeze(0)
+#         action = agent.select_action(state).cpu().numpy()
+
+#         obs, reward, terminated, truncated, info = env.step(action)
+#         done = terminated or truncated
+
+# env.close()
---
+++ train and eval.py
@@ -0,0 +1,79 @@
+import ddpg
+import gymnasium as gym
+from gymnasium.wrappers import RecordEpisodeStatistics, RecordVideo
+
+import math
+import os
+import random
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cpu")
+# Hyperparameters
+N_EPISODES = 200
+BATCH_SIZE = 128
+MEMORY_SIZE = 1000000
+GAMMA = 0.99
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 100
+WEIGHT_DECAY = 0.005
+TAU = 1e-3
+LR = 1e-4
+
+env = gym.make("HumanoidStandup-v5", render_mode="rgb_array", max_episode_steps=200)
+
+n_actions = env.action_space.shape[0] 
+state, _ = env.reset()
+n_obs = len(state)
+
+# Training loop
+agent = ddpg.DDPG(n_obs, n_actions, BATCH_SIZE, GAMMA, TAU, LR, WEIGHT_DECAY)
+criterion = nn.MSELoss()
+memory = ddpg.ReplayMemory(MEMORY_SIZE)
+losses = []
+
+progress_bar = tqdm(total=N_EPISODES, position=0, leave=True)
+for i_episode in range(N_EPISODES):
+    progress_bar.update(1)
+    episode_reward = ddpg.train(i_episode, EPS_START, EPS_END, EPS_DECAY, criterion, agent, memory, env)
+    losses.append(episode_reward)
+    if i_episode>0:
+        progress_bar.set_postfix({
+                'last reward': round(episode_reward, 5),
+                'best reward': round(max(losses), 5),
+                })
+    
+del progress_bar
+
+print("Training completed.")
+plt.plot(losses)
+plt.xlabel("Episodes")
+plt.ylabel("Training Reward")
+plt.show()
+
+print("Saving model...")
+agent.save("humanoid")
+print("Saved Model Weights!")
+
+num_eval_episodes=5
+env = RecordEpisodeStatistics(env, buffer_length=num_eval_episodes)
+env = RecordVideo(env, video_folder="video_renders", name_prefix="cheetah", episode_trigger=lambda _: True)
+
+for episode_num in range(num_eval_episodes):
+    obs, _ = env.reset()
+    done = False
+
+    while not done:
+        state = torch.tensor(obs, dtype=torch.float32, device=device).unsqueeze(0)
+        action = agent.select_action(state).cpu().numpy()
+
+        obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
+
+env.close()
